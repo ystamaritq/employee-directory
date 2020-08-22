788417a0fad71d94477a02219aa60577c7e1912f
Yadira Tamarit <ystamaritq@gmail.com>
2020-08-21 22:49:02 -0500

    adjust a callback function on Search component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
+import SearchTable from "./components/SearchTable/SearchTable";
+import testData from "./test-data.json";
 
 function App() {
+	const data = testData.results;
 	return (
 		<div className="App">
 			<Header
 				title="Employee Directory"
 				subtitle="Click in the arrow to filter the results"
 			/>
-			<Search name="Yadira" />
+			<Search name="" onValueChange={(value) => console.log(value)} />
+			<SearchTable data={data} />
 		</div>
 	);
 }
